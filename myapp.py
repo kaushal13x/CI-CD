9fe3458e0c65f2a2f9a25adf9a5a155b475ff86c
root <root@localhost.localdomain>
2025-07-18 07:34:11 -0400

    second

--- myapp.py
+++ myapp.py
@@ -1,8 +1,12 @@
 from flask import Flask 
-app = Flask(name)
+
+app = Flask(__name__)
+
 @app.route("/")
-def home()
+
+def home():
     return "Hello From Deployed Via Jenkins & Docker Kaushal Kumar"
-if name == "main":
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
 
